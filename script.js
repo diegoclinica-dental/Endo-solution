370146aca2c076f19cdcdacbc18fdd06087b60d3
Endo solution - Especialidades Odontológicas <diego_rene1993@hotmail.com>
2025-07-22 12:23:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// Menú móvil
+const mobileMenu = document.getElementById('mobile-menu');
+const navbar = document.getElementById('navbar');
+
+mobileMenu.addEventListener('click', () => {
+  navbar.classList.toggle('active');
+});
 document.getElementById("formulario-contacto").addEventListener("submit", function(e) {
   e.preventDefault();
   alert("Gracias por tu mensaje. Nos pondremos en contacto contigo pronto.");
